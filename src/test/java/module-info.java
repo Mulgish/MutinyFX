006f038c29d430f85dd25835e4b788883948dc28
Juri Duval <irlite@tuta.io>
2022-03-11 21:38:22 +0000

    Fixing coverage

---
+++ src/test/java/module-info.java
@@ -0,0 +1,16 @@
+module com.test {
+    requires com.mulgish.mutinyfx;
+    requires org.junit.platform.engine;
+    requires org.junit.jupiter.engine;
+    requires org.junit.jupiter.api;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires io.smallrye.mutiny;
+    requires org.testfx.junit5;
+    requires org.testfx.monocle;
+    requires org.testfx;
+
+    exports com.test;
+    opens com.test;
+}
